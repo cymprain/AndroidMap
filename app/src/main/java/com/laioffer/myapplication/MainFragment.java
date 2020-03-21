2bbf3084666451db963278422ac21e4e22df6ad1
cymprain <yingchen199@gmail.com>
2020-03-21 14:08:18 -0700

    Add like to event and firebase cloud messaging functions.

--- app/src/main/java/com/laioffer/myapplication/MainFragment.java
+++ app/src/main/java/com/laioffer/myapplication/MainFragment.java
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -411,6 +412,24 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
         mEventTextLike.setText(String.valueOf(likeNumber));
         mEventTextType.setText(type);
 
+        final String url = mEvent.getImgUri();
+        mEventImageType.setImageDrawable(ContextCompat.getDrawable(getContext(), Config.trafficMap.get(type)));
+        if (url != null) {
+            new AsyncTask<Void, Void, Bitmap>() {
+                @Override
+                protected Bitmap doInBackground(Void... voids) {
+                    Bitmap bitmap = Utils.getBitmapFromURL(url);
+                    return bitmap;
+                }
+
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    super.onPostExecute(bitmap);
+                    mEventImageType.setImageBitmap(bitmap);
+                }
+            }.execute();
+        }
+
         mEventImageType.setImageDrawable(ContextCompat.getDrawable(getContext(), Config.trafficMap.get(type)));
 
         if (user == null) {
@@ -455,6 +474,16 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
         mEventTextType = (TextView) view.findViewById(R.id.event_info_type_text);
         mEventTextLocation = (TextView) view.findViewById(R.id.event_info_location_text);
         mEventTextTime = (TextView) view.findViewById(R.id.event_info_time_text);
+
+        mEventImageLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int number = Integer.parseInt(mEventTextLike.getText().toString());
+                database.child("events").child(mEvent.getId()).child("event_like_number").setValue(number + 1);
+                mEventTextLike.setText(String.valueOf(number + 1));
+            }
+        });
+
     }
 
 
